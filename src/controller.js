6e75968fe1e3b15b89ac167c0b0fd2cfa5d54406
maidanaen <maidanaenzosebastian@gmail.com>
2023-10-23 21:09:50 -0300

    manejo de errores

--- src/controller.js
+++ src/controller.js
@@ -3,29 +3,79 @@ import { pool } from "./database.js";
 class BibliotecaController{
     
     async getALL(req, res){
-        const [result] = await pool.query('SELECT * FROM libros');
-        res.json(result);
+        try {
+          const [result] = await pool.query('SELECT * FROM libros');
+            if (result.length === 0) {
+            res.status(404).json({ error: 'No hay libros en la base de datos' });
+        } else {
+            res.json(result);
+        }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error al buscar los libros' });
+        }
+    }   
+    async getOne(req, res) {
+        try {
+            const libro = req.body;
+            const [result] = await pool.query(`SELECT * FROM libros WHERE ISBN= (?)`, [libro.ISBN]);
+            if (result.length == 0) {
+            res.status(404).json({ error: 'El libro no existe' });
+        } else {
+            res.json(result);
+        }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error al buscar el libro' });
+        }
     }
-    async getOne (req, res){
-        const libro =req.body;
-        const [result]= await pool.query(`SELECT * FROM libros WHERE id= (?)`,[libro.id]);
-        res.json(result);
-    }
+    
     async add(req, res){
-        const libro=req.body;
-        const [result]= await pool.query(`INSERT INTO libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES(? , ? , ?, ?, ?)`,[libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
-        res.json({"libro cargado correctamente": result.insertId});
+        try {
+            const libro=req.body;
+            if (!libro.nombre || !libro.autor || !libro.categoria || !libro.año_publicacion || !libro.ISBN) {
+            res.status(400).json({ error: 'Faltan campos requeridos' });
+        } else {
+            const [result]= await pool.query(`INSERT INTO libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES(? , ? , ?, ?, ?)`,[libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
+            res.json({"libro cargado correctamente": result.insertId});
+        }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error al cargar el libro' });
+        }
     }
+    
     async update(req, res){
-        const libro=req.body;
-        const [result]= await pool.query(`UPDATE  libros SET nombre= (?), autor= (?), categoria= (?), año_publicacion= (?), ISBN= (?) WHERE id= (?)`,[libro.nombre,libro.autor,libro.categoria,libro.año_publicacion, libro.ISBN, libro.id]);
-        res.json({"libros actualizados correctamente": result.changedRows});
+        try {
+            const libro=req.body;
+            const [result]= await pool.query(`UPDATE  libros SET nombre= (?), autor= (?), categoria= (?), año_publicacion= (?) WHERE ISBN= (?) `,[libro.nombre,libro.autor,libro.categoria,libro.año_publicacion, libro.ISBN]);
+            if (result.changedRows == 0) {
+            res.status(404).json({ error: 'El libro no existe' });
+        } else {
+            res.json({"Libros actualizados correctamente": result.changedRows});
+        }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error al actualizar el libro' });
+        }
     }
+    
     async delete(req, res){
-        const libro=req.body;
-        const [result]= await pool.query(`DELETE FROM libros WHERE ISBN = (?)`,[libro.ISBN]);
-        res.json({"libros eliminados correctamente": result.affectedRows});
+        try {
+            const libro=req.body;
+            const [result]= await pool.query(`DELETE FROM libros WHERE ISBN = (?)`,[libro.ISBN]);
+        if (result.affectedRows == 0) {
+            res.status(404).json({ error: 'El libro no existe' });
+        } else {
+            res.json({"Libros eliminados correctamente": result.affectedRows});
+        }
+        } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al eliminar el libro' });
+        }
     }
+      
+    
 }
 
 export const libro = new BibliotecaController();
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import {libro } from './controller.js';
 
 export const router = Router ();
 router.get('/libros', libro.getALL);
-router.get('/lib', libro.getOne);
+router.get('/buscar', libro.getOne);
 router.post('/libro_nuevo', libro.add);
 router.put('/actualizar', libro.update);
 router.delete('/eliminar',libro.delete);
